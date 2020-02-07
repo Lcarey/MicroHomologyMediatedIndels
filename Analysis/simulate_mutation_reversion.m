b6f1dc019f9989536368fcc8fedbffc9cdf85486
Lucas Carey <lucas.carey@gmail.com>
2020-02-07 11:19:24 +0800

    a

---
+++ Analysis/simulate_mutation_reversion.m
@@ -0,0 +1,10 @@
+ function mutant_frequency = simulate_mutation_reversion(number_of_generations,mutation_rate,reversion_rate)
+    WTfreq = 1 ;
+    mutant_frequency = NaN( 1 , number_of_generations );
+    mutant_frequency(1) = 1-WTfreq ;
+    for I = 2:number_of_generations
+        mutant_frequency(I) = mutant_frequency(I-1)  + ...
+        (1-mutant_frequency(I-1)) * mutation_rate - reversion_rate*mutant_frequency(I-1) ;
+    end
+
+end
